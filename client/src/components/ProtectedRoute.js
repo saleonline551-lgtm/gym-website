8049166b95a137791b73365071de07f6c895edd3
MohdFayyaz <mohdfayyazmca@gmail.com>
2026-05-15 12:08:52 +0530

    Fixed Google Protected Route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,15 +1,23 @@
 import React from "react";
+
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
 
   const token = localStorage.getItem("token");
 
-  if (!token) {
+  const user = localStorage.getItem("user");
+
+  // NORMAL LOGIN OR GOOGLE LOGIN
+
+  if (!token && !user) {
+
     return <Navigate to="/login" />;
+
   }
 
   return children;
+
 }
 
 export default ProtectedRoute;
